6f69c3bbc2eec2a2bce1b4353fa5596ea999bfcb
hoshisora000 <hoshisora000@gmail.com>
2023-03-26 04:09:51 +0800

    add button

--- app/src/main/java/com/example/hw1/MainActivity.kt
+++ app/src/main/java/com/example/hw1/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.hw1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.button)
+        button1.setOnClickListener {
+            startActivity(Intent(this,MainActivity2::class.java))
+        }
+
     }
 }
--- app/src/main/java/com/example/hw1/MainActivity2.kt
+++ app/src/main/java/com/example/hw1/MainActivity2.kt
@@ -1,11 +1,18 @@
 package com.example.hw1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
